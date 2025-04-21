03ccb7509cc0bdf1a7817122eac380db0652d1ff
Null <117316014+ltseverydayyou@users.noreply.github.com>
2025-04-21 12:41:10 +0300

    Update NA testing.lua

--- NA testing.lua
+++ NA testing.lua
@@ -15623,11 +15623,6 @@ Spawn(function() -- init
 end)
 
 Spawn(bindToDevConsole)
-Spawn(function()
-	if game:GetService("Players").LocalPlayer.UserId==3101266219 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/uuuuuuu/refs/heads/main/thefuni.lua"))();
-	end
-end)
 
 Spawn(function()
 	NACaller(function()--better saveinstance support
